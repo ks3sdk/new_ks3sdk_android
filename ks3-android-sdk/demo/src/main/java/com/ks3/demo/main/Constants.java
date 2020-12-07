41dd1e6960d3473f5f57420dcf593d9933500f06
cqc <cqclife@qq.com>
2020-12-07 18:13:55 +0800

    【fixbug】 1、配置变量参数增加

--- ks3-android-sdk/demo/src/main/java/com/ks3/demo/main/Constants.java
+++ ks3-android-sdk/demo/src/main/java/com/ks3/demo/main/Constants.java
@@ -1,9 +1,9 @@
 package com.ks3.demo.main;
 
 /**
- * 
+ *
  * Demo中一些常量
- * 
+ *
  */
 public class Constants {
 	// public static final String ACCESS_KEY__ID = "YOUR_ACCESS_KEY_ID";
@@ -12,8 +12,8 @@ public class Constants {
 	public static final String TEST_IMG = "YOUR_TEST_IMG_IN_SDCARD_ROOT_PATH";
 	protected static final String UPLOAD_ID = "YOUR_UPLOAD_ID";
 	// public static final String APP_SERTVER_HOST =
-	// "http://192.168.231.49:11911";
-	public static final String TEST_MULTIUPLOAD_FILE = "/sdcard/11.txt";
+	// "http://192.168.231.49:119
+	public static final String TEST_MULTIUPLOAD_FILE = "/storage/emulated/app/1.doc";
 	public static final String TEST_MULTIUPLOAD_BUCKET = "aaabbbccc";
 
 	public static final String APP_SERTVER_HOST = "YOUR_APP_SERVER_HOST";
@@ -22,4 +22,10 @@ public class Constants {
 	public static final String ACCESS_KEY_SECRET = "";
 	public static final String END_POINT = "";
 	public static final long MULTI_UPLOAD_THREADHOLD = 1 * 1024 * 1024;
+
+	public static final int  READ_TIMEOUT = 3000;
+
+	public static final String SRC_BUCKETNAME = "";
+	public static final String DST_BUCKETNAME = "";
+
 }
